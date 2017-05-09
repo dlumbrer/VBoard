6fe08d2fa67620f0dc4df7c8290bc22afef379a7
David Moreno Lumbreras <dmorenolumb@gmail.com>
2017-05-09 10:47:34 +0200

    Add panels to dashboard

--- app/controller/DashboardController.js
+++ app/controller/DashboardController.js
@@ -70,6 +70,72 @@ define(
 
 					}////////////////////////////////////////////////
 
+					//////////////////////////////////////AÑADIR VIS A PANEL//////////////////////
+					var posx = 100;
+					$scope.addPanelToDash = function(paneltoadd){
+						console.log("A AÑADIR", paneltoadd);
+
+						var dimension = "[" + paneltoadd._source.dimension + "]"
+
+						var panel = THREEDC.Panel({numberOfRows:parseInt(paneltoadd._source.rows), numberOfColumns:paneltoadd._source.columns}, JSON.parse(dimension), JSON.parse(paneltoadd._source.opacity));
+
+						dash.addPanel(panel, {x:posx, y:100, z:100})
+						posx += 200;
+
+						for (var i = 0; i < paneltoadd._source.charts.length; i++) {
+							addVisToPanel(panel, paneltoadd._source.charts[i])
+						}
+
+					}
+
+					var addVisToPanel = function(panel, visall){
+						console.log("Hay que meter esta vis en el panel", visall);
+
+						var rowchart = visall.row;
+						var rowcolumn = visall.column;
+
+						var promise = generatorQueries.getVis(ESService.client, visall.id);
+						promise.then(function (resp) {
+							console.log("Visualiazcion a editar: ", resp.hits.hits[0])
+							vis = resp.hits.hits[0]._source;
+
+
+
+							switch (vis.chartType) {
+									case "pie":
+											var chart = THREEDC.pieChart().data(vis.visobject._data).id(visall.id);
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break
+									case "bars":
+											var chart = THREEDC.barsChart().data(vis.visobject._data).id(visall.id);
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break;
+									case "line":
+											var chart = THREEDC.lineChart().data(vis.visobject._data).id(visall.id);
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break;
+									case "curve":
+											var chart = THREEDC.smoothCurveChart().data(vis.visobject._data).id(visall.id);
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break;
+									case "3DBars":
+											var chart = THREEDC.TDbarsChart().data(vis.visobject._data).id(visall.id).gridsOn();
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break;
+									case "bubbles":
+											var chart = THREEDC.bubbleChart().data(vis.visobject._data).id(visall.id).gridsOn();
+											panel.addChart(chart, {row:rowchart, column: rowcolumn})
+											break;
+									default:
+											console.log("Esta vacío")
+											return
+
+							}
+						})
+					}
+
+					////////////////////////////////////////////////
+
 
 					///////////////////////////////////////////THREEDC/////////////////////////////////////////
 					var container = document.getElementById( 'ThreeJSDashboard' );
